bec1c2e035d877c1cdd5bce912c15f75799c1dea
ewzhu <ewzhu2002@gmail.com>
2026-01-19 22:04:54 -0500

    This is correct code for everything, but not formatted into graphs yet.

--- NashEquilibria.py
+++ NashEquilibria.py
@@ -1,4 +1,7 @@
 import sys
+import math
+import matplotlib.pyplot as plt
+from Player import Player
 file = sys.argv[1]
 inputArr = [] 
 
@@ -12,12 +15,9 @@ with open(file, 'r') as file:
         li = line.strip()
         if li != "" and not li.startswith("#"):
             filtered_lines.append(li)
-            print(filtered_lines)
-    print(filtered_lines)
     for line in filtered_lines:
         n = int(filtered_lines[0])
         li = line.strip()
-        print(li)
         words = li.split()
         choices.append(words[0])
         for i in range(1, len(words)-1, n):
@@ -27,6 +27,66 @@ with open(file, 'r') as file:
                 payoff.append(int_array)
             except:
                 break
-
 print(payoff)
-print(choices)
+p1 = Player(0, 2)
+p2 = Player(1, 2)
+P1y = []
+P1x = []
+P2y = []
+P2x = []
+for i in range(300):
+    P1y.append(p1.getPrefs()[0])
+    P2y.append(p2.getPrefs()[1])
+    p1strat = p1.getStrategy()
+    p2strat = p2.getStrategy()
+    p2.updateScore(p1strat, payoff)
+    p2.updatePrefs()
+    p1.updateScore(p2strat, payoff)
+    p1.updatePrefs()
+for P1yVal in P1y:
+    P1x.append(1-P1yVal) # generate the x value as 1-y
+
+# P1 Sizes (Dummy Data)
+P1sizes = [-200*math.log(1 - x) for x in P1y]
+P2sizes = [-200*math.log(1 - x) for x in P2y]
+# P2 Preferences (Dummy Data)
+
+for P2yVal in P2y:
+    P2x.append(1-P2yVal) # generate the x value as 1-y
+
+# P2 Sizes (Dummy Data)
+
+
+# plot
+fig, ax = plt.subplots()
+ax.scatter(P1x, P1y, s=P1sizes, c='blue', label='P1', alpha=0.3)
+ax.scatter(P2x, P2y, s=P2sizes, c='orange', label='P2', alpha=0.4)
+
+# labels and title
+ax.set_xlabel('quiet', fontsize=10)
+ax.set_ylabel('confess', fontsize=10)
+ax.set_title('Prisoner\'s Dilemma')
+
+# show grid
+ax.grid(True)
+
+# create some mock scatterpoints for the legend, otherwise
+# the circles in the legend are too big
+# this seems like not the best way to do it but i'm tired
+p1LegendX = [0.5]
+p1LegendY = [0.5]
+p1LegendSize = [50]
+p1CollectionLegend = ax.scatter(p1LegendX, p1LegendY, s=p1LegendSize, c='blue', alpha=0.3)
+
+p2LegendX = [0.5]
+p2LegendY = [0.5]
+p2LegendSize = [50]
+p2CollectionLegend = ax.scatter(p2LegendX, p2LegendY, s=p2LegendSize, c='orange', alpha=0.4)
+
+plt.legend([p1CollectionLegend, p2CollectionLegend], ['P1', 'P2'])
+# end legend code
+
+# show plot
+plt.show()
+
+
---
+++ Player.py
@@ -0,0 +1,75 @@
+
+# SahuPlayer.py
+# Author sahwho
+'''
+    Represent a Player for Mod/Sim Fall 2024 Project 1 (MBHS)
+    A Player has an id (like "P1"), as well as preferences for 2 strategies (which sum to 1.0).
+    and keeps track of how many games they've played, plus their total score and average score.
+    If you use this class, feel free to rename it Player.py
+
+    Inspiration drawn from the work done by Dr. Andrew Davison (PSU)
+
+    sample usage: p1 = SahuPlayer("P1")
+'''
+
+import random
+
+class Player:
+    def __init__(self, id, n):
+        self.id = int(id)
+        self.prefs = [] # strategy1, strategy2 #edit this to take in 3 strats
+        self.n = n
+        for i in range(n):
+            self.prefs.append(1.0/n)
+        self.currentGameScore = 0
+        self.numGamesPlayed = 0.0
+        self.totalScore = 0;
+        self.averageScore = 0
+
+    def getId(self):
+        return self.id
+
+    def getPrefs(self):
+        return self.prefs
+
+    # no getters needed for currentGameScore, numGamesPlayed, totalScore, or averageScore,
+    # since those are used internally within these methods
+    # todo: based on the preferences, pseduorandomly select strategy1 or strategy2
+    def getStrategy(self):
+        strategyIndices = range(0, len(self.prefs))
+        return int(random.choices(strategyIndices, weights = self.prefs, k =1)[0])
+        
+    # todo: update totalScore, numGamesPlayed, and averageScore
+    # this method assumes that a game has been played
+    def updateScore(self, opponentStrat, arr):
+        strategy = self.getStrategy()
+        score = arr[strategy*self.n+opponentStrat][int(self.id)]
+        self.currentGameScore = score
+        self.numGamesPlayed +=1
+        self.totalScore += self.currentGameScore
+        self.averageScore = self.totalScore/self.numGamesPlayed
+    # todo: use a formula to update the current Player's preferences
+    #Better updateprefs formula cuz original one went negative
+    def updatePrefs(self):
+        factor = 1.05  
+        for i in range(len(self.prefs)):
+            if i == self.getStrategy():
+                self.prefs[i] *= factor
+            else:
+                self.prefs[i] /= factor
+
+    # normalize so sum = 1
+        total = sum(self.prefs)
+        self.prefs = [p / total for p in self.prefs]
+    '''
+    def updatePrefs(self):
+        strat = self.getStrategy()
+        for i in range(len(self.prefs)):
+            if i != strat:#updates rest of the preferences
+                self.prefs[i] = self.prefs[i] - (self.currentGameScore - self.averageScore)/(100*(len(self.prefs)-1))
+            else:
+                self.prefs[i] = self.prefs[i] + (self.currentGameScore - self.averageScore)/100
+    '''
+    # format the object for printing: P1: <0.5, 0.5>
+    def __str__(self):
+        return str(self.id) + ': <' + str(self.prefs[0]) + ',' + str(self.prefs[1]) + '>'
