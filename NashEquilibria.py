6360ae1ef4eddbe67ce4fe1afa7b25a91c1181d6
ewzhu <ewzhu2002@gmail.com>
2026-01-22 15:03:29 -0500

    These are the completed functions with graphs, but without comments

--- NashEquilibria.py
+++ NashEquilibria.py
@@ -1,12 +1,17 @@
 import sys
 import math
 import matplotlib.pyplot as plt
+import numpy as np
+import ternary
+
+from collections import Counter
 from Player import Player
 file = sys.argv[1]
 inputArr = [] 
 
 payoff = []
 choices = []
+firstLine = ""
 print("Reading " + str(file))
 with open(file, 'r') as file:
     lines = file.readlines()
@@ -20,73 +25,130 @@ with open(file, 'r') as file:
         li = line.strip()
         words = li.split()
         choices.append(words[0])
-        for i in range(1, len(words)-1, n):
+        for i in range(1, len(words)-1, 2):
             try:
-                onePayoff = words[i:i+n]
+                onePayoff = words[i:i+2]
                 int_array = list(map(int, onePayoff))
                 payoff.append(int_array)
             except:
                 break
+    firstLine = filtered_lines[1]
 print(payoff)
-p1 = Player(0, 2)
-p2 = Player(1, 2)
-P1y = []
-P1x = []
-P2y = []
-P2x = []
-for i in range(300):
-    P1y.append(p1.getPrefs()[0])
-    P2y.append(p2.getPrefs()[1])
-    p1strat = p1.getStrategy()
-    p2strat = p2.getStrategy()
-    p2.updateScore(p1strat, payoff)
-    p2.updatePrefs()
-    p1.updateScore(p2strat, payoff)
-    p1.updatePrefs()
-for P1yVal in P1y:
-    P1x.append(1-P1yVal) # generate the x value as 1-y
+print(choices)
+def playGame(gameName, player1, player2, xLabel, yLabel, n):
+    if n == 2:
+        p1 = Player(0, 2)
+        p2 = Player(1, 2)
+        P1y = []
+        P1x = []
+        P2y = []
+        P2x = []
+        count = []
+        for i in range(500):
+            P1y.append(p1.getPrefs()[0])
+            P2y.append(p2.getPrefs()[0])
+            p1strat = p1.getLastStrategy()
+            p2strat = p2.getLastStrategy()
+            p2.updateScore(p1strat, payoff)
+            p2.updatePrefs()
+            p1.updateScore(p2strat, payoff)
+            p1.updatePrefs()
+        for P1yVal in P1y:
+            P1x.append(P1yVal) # generate the x value as 1-y
+        for P2yVal in P2y:
+            P2x.append(P2yVal) # generate the x value as 1-y
 
-# P1 Sizes (Dummy Data)
-P1sizes = [-200*math.log(1 - x) for x in P1y]
-P2sizes = [-200*math.log(1 - x) for x in P2y]
-# P2 Preferences (Dummy Data)
+        # P2 Sizes (Dummy Data)
+        sizes1 = np.histogram(P1y, bins = 20, range=(0,1))[0]
+        P1sizes = [100*sizes1[i] for i in range(len(sizes1))]
+        sizes2 = np.histogram(P1y, bins = 20, range=(0,1))[0]
+        P2sizes = [100*sizes1[i] for i in range(len(sizes1))]
+        # plot
+        fig, ax = plt.subplots()
+        ax.scatter([i/20 for i in range(20)],  [(20-i)/20 for i in range(20)], s=P1sizes, color='blue', label='P1', alpha=0.3)
+        ax.scatter([i/21 for i in range(20)],  [(21-i)/21 for i in range(20)], s=P2sizes, color='orange', label='P2', alpha=0.4)
 
-for P2yVal in P2y:
-    P2x.append(1-P2yVal) # generate the x value as 1-y
+        # labels and title
+        ax.set_xlabel(xLabel, fontsize=10)
+        ax.set_ylabel(yLabel, fontsize=10)
+        ax.set_title(gameName)
 
-# P2 Sizes (Dummy Data)
+        # show grid
+        ax.grid(True)
 
+        # create some mock scatterpoints for the legend, otherwise
+        # the circles in the legend are too big
+        # this seems like not the best way to do it but i'm tired
+        p1LegendX = [0.5]
+        p1LegendY = [0.5]
+        p1LegendSize = [50]
+        p1CollectionLegend = ax.scatter(p1LegendX, p1LegendY, s=p1LegendSize, c='blue', alpha=0.3)
 
-# plot
-fig, ax = plt.subplots()
-ax.scatter(P1x, P1y, s=P1sizes, c='blue', label='P1', alpha=0.3)
-ax.scatter(P2x, P2y, s=P2sizes, c='orange', label='P2', alpha=0.4)
+        p2LegendX = [0.5]
+        p2LegendY = [0.5]
+        p2LegendSize = [50]
+        p2CollectionLegend = ax.scatter(p2LegendX, p2LegendY, s=p2LegendSize, c='orange', alpha=0.4)
+        plt.legend([p1CollectionLegend, p2CollectionLegend], [player1, player2])
+        # end legend code
+        # show plot
+        plt.show()
+    if n == 3:
+        p1 = Player(0, 3)
+        p2 = Player(1, 3)
+        P1y = []
+        P1x = []
+        P2y = []
+        P2x = []
+        count = []
+        history1 = []
+        history2 = []
+        for i in range(100):
+            P1y.append(p1.getPrefs()[0])
+            P2y.append(p2.getPrefs()[0])
+            p1strat = p1.getLastStrategy()
+            p2strat = p2.getLastStrategy()
+            p2.updateScore(p1strat, payoff)
+            p2.updatePrefs()
+            p1.updateScore(p2strat, payoff)
+            p1.updatePrefs()
+            history1.append([p1.getPrefs()[0], p1.getPrefs()[1], p1.getPrefs()[2]]) # generate the x value as 1-y
+            history2.append([p1.getPrefs()[0], p1.getPrefs()[1], p1.getPrefs()[2]]) # generate the x value as 1-y
+        # P2 Sizes (Dummy Data)
+        
+        #sizes1 = np.histogramdd([[history[i][j] for i in range(len(history))] for j in range(len(history[0]))], bins = 20, range=[(0,1), (0,1), (0,1)])[0]
+        ##sizes1 = [[500*sizes1[j][i] for i in range(len(sizes1[0]))] for j in range(len(sizes1))]
+        # plot
+        # create some mock scatterpoints for the legend, otherwise
+        # the circles in the legend are too big
+        # this seems like not the best way to do it but i'm tired
 
-# labels and title
-ax.set_xlabel('quiet', fontsize=10)
-ax.set_ylabel('confess', fontsize=10)
-ax.set_title('Prisoner\'s Dilemma')
+        ## Boundary and Gridlines
+        scale = 1
+        figure, tax = ternary.figure(scale=scale)
+        # Draw Boundary and Gridlines
+        tax.boundary(linewidth=2.0)
+        tax.gridlines(color="black", multiple=5)
+        tax.gridlines(color="blue", multiple=1, linewidth=0.5)
+        # Set Axis labels and Title
+        fontsize = 20
+        tax.set_title("Rock Paper Scissors", fontsize=fontsize)
+        tax.left_axis_label("Rock", fontsize=fontsize)
+        tax.right_axis_label("Paper", fontsize=fontsize)
+        tax.bottom_axis_label("Scissors", fontsize=fontsize)
 
-# show grid
-ax.grid(True)
+        # Set ticks
+        tax.ticks(axis='lbr', linewidth=1)
+        p1Scatter = tax.scatter(history1, s = 100, c='blue', alpha=0.2, label=str(player1) + " = blue")
+        p2Scatter = tax.scatter(history2, s = 100, c = 'orange', alpha = 0.07, label=str(player2) + " = orange")#less alpha so that the blue actually shows up
 
-# create some mock scatterpoints for the legend, otherwise
-# the circles in the legend are too big
-# this seems like not the best way to do it but i'm tired
-p1LegendX = [0.5]
-p1LegendY = [0.5]
-p1LegendSize = [50]
-p1CollectionLegend = ax.scatter(p1LegendX, p1LegendY, s=p1LegendSize, c='blue', alpha=0.3)
+        # Remove default Matplotlib Axes
+        tax.clear_matplotlib_ticks()
+        tax.legend()
 
-p2LegendX = [0.5]
-p2LegendY = [0.5]
-p2LegendSize = [50]
-p2CollectionLegend = ax.scatter(p2LegendX, p2LegendY, s=p2LegendSize, c='orange', alpha=0.4)
-
-plt.legend([p1CollectionLegend, p2CollectionLegend], ['P1', 'P2'])
-# end legend code
-
-# show plot
-plt.show()
-
+        ternary.plt.show()
+playGame(firstLine, "Player1", "Player2", choices[2], choices[3], int(choices[0]))
+playGame(firstLine, "Player3", "Player4", choices[2], choices[3], int(choices[0]))
+playGame(firstLine, "Player5", "Player6", choices[2], choices[3], int(choices[0]))
+playGame(firstLine, "Player7", "Player8", choices[2], choices[3], int(choices[0]))
+playGame(firstLine, "Player9", "Player10", choices[2], choices[3], int(choices[0]))
 
--- Player.py
+++ Player.py
@@ -23,9 +23,9 @@ class Player:
             self.prefs.append(1.0/n)
         self.currentGameScore = 0
         self.numGamesPlayed = 0.0
-        self.totalScore = 0;
+        self.totalScore = 0
         self.averageScore = 0
-
+        self.lastStrategy = 0
     def getId(self):
         return self.id
 
@@ -38,18 +38,20 @@ class Player:
     def getStrategy(self):
         strategyIndices = range(0, len(self.prefs))
         return int(random.choices(strategyIndices, weights = self.prefs, k =1)[0])
-        
+    def getLastStrategy(self):
+        return self.lastStrategy
     # todo: update totalScore, numGamesPlayed, and averageScore
     # this method assumes that a game has been played
     def updateScore(self, opponentStrat, arr):
-        strategy = self.getStrategy()
-        score = arr[strategy*self.n+opponentStrat][int(self.id)]
+        self.lastStrategy = self.getStrategy()
+        score = arr[self.lastStrategy*self.n+opponentStrat][int(self.id)]
         self.currentGameScore = score
         self.numGamesPlayed +=1
         self.totalScore += self.currentGameScore
         self.averageScore = self.totalScore/self.numGamesPlayed
     # todo: use a formula to update the current Player's preferences
     #Better updateprefs formula cuz original one went negative
+    '''
     def updatePrefs(self):
         factor = 1.05  
         for i in range(len(self.prefs)):
@@ -57,19 +59,26 @@ class Player:
                 self.prefs[i] *= factor
             else:
                 self.prefs[i] /= factor
-
     # normalize so sum = 1
         total = sum(self.prefs)
         self.prefs = [p / total for p in self.prefs]
-    '''
+        '''
     def updatePrefs(self):
-        strat = self.getStrategy()
+        strat = self.lastStrategy
         for i in range(len(self.prefs)):
             if i != strat:#updates rest of the preferences
                 self.prefs[i] = self.prefs[i] - (self.currentGameScore - self.averageScore)/(100*(len(self.prefs)-1))
             else:
                 self.prefs[i] = self.prefs[i] + (self.currentGameScore - self.averageScore)/100
-    '''
+        for i in range(len(self.prefs)):
+            if self.prefs[i] < 0:
+                self.prefs[i] = 0
+            elif self.prefs[i] >1:
+                self.prefs[i] = 1
+        total = sum(self.prefs)
+        for i in range(len(self.prefs)):
+            self.prefs[i] = self.prefs[i] / total
     # format the object for printing: P1: <0.5, 0.5>
+
     def __str__(self):
         return str(self.id) + ': <' + str(self.prefs[0]) + ',' + str(self.prefs[1]) + '>'
